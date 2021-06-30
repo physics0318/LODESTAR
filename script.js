220603d602a7e9b39cf395acde8d1d7551fc292f
Your Name <you@example.com>
2021-06-30 17:18:25 +0900

    디자인 및 폰트 수정

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 var magazine = document.getElementById("showmagazine").getBoundingClientRect().top;
+var first_expln = document.getElementById("first_expln").getBoundingClientRect().top;
+var second_expln = document.getElementById("second_expln").getBoundingClientRect().top;
 
-var scroll_to = {"what": magazine, "which": magazine, "since": magazine};
+var scroll_to = {"what": first_expln, "which": second_expln, "since": magazine};
 
 function nav_click(clicked_id) {
     window.scrollTo({top: scroll_to[clicked_id], left: 0, behavior: "smooth"});
 }
+
+// var trigger = 20;
+
+// document.addEventListener("scroll", () => {
+//     const posY = window.scrollY;
+    
+//     if (posY >= second_expln) {
+
+//     }
+// })
