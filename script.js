9820cee51832ebd54402ac8a468cfcc77ca0b984
Your Name <you@example.com>
2021-07-02 15:38:32 +0900

    자바스크립트 애니메이션 추가

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const nav_height = document.getElementById("nav_bar").offsetHeight;
+
 var magazine = document.getElementById("showmagazine").getBoundingClientRect().top;
 var first_expln = document.getElementById("first_expln").getBoundingClientRect().top;
 var second_expln = document.getElementById("second_expln").getBoundingClientRect().top;
+var sections = [first_expln, second_expln, magazine];
+var articles = ["first_artcl", "second_artcl", "magazine_cards"];
 
-var scroll_to = {"what": first_expln, "which": second_expln, "since": magazine};
+var scroll_to = {"what": sections[0], "which": sections[1], "since": sections[2]};
 
 function nav_click(clicked_id) {
-    window.scrollTo({top: scroll_to[clicked_id], left: 0, behavior: "smooth"});
+    window.scrollTo({top: scroll_to[clicked_id] - nav_height - 20, left: 0, behavior: "smooth"});
 }
 
-// var trigger = 20;
 
-// document.addEventListener("scroll", () => {
-//     const posY = window.scrollY;
-    
-//     if (posY >= second_expln) {
-
-//     }
-// })
+document.addEventListener("scroll", () => {
+    const trigger = 50;
+    var posY = window.scrollY;
+    for (var i = 0; i < sections.length; i++) {
+        var elm = document.getElementById(articles[i]);
+        if (posY >= sections[i] - trigger - nav_height) {
+            elm.style.transition = "opacity 0.3s linear 0s";
+            elm.style.opacity = "95%";
+            if (articles[i] === "magazine_cards") { //opacity 100%인 요소들
+                elm.style.opacity = "100%";
+            }
+        } else{
+            elm.style.opacity = "0%";
+        }
+    }
+})
