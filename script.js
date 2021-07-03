9282f6512ad361ccd060792b87a0947f676b384e
Your Name <you@example.com>
2021-07-03 15:49:23 +0900

    footer 추가

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const nav_height = document.getElementById("nav_bar").offsetHeight;
+const magazine = document.getElementById("showmagazine").getBoundingClientRect().top;
+const first_expln = document.getElementById("first_expln").getBoundingClientRect().top;
+const second_expln = document.getElementById("second_expln").getBoundingClientRect().top;
 
-var magazine = document.getElementById("showmagazine").getBoundingClientRect().top;
-var first_expln = document.getElementById("first_expln").getBoundingClientRect().top;
-var second_expln = document.getElementById("second_expln").getBoundingClientRect().top;
 var sections = [first_expln, second_expln, magazine];
 var articles = ["first_artcl", "second_artcl", "magazine_cards"];
-
 var scroll_to = {"what": sections[0], "which": sections[1], "since": sections[2]};
 
 function nav_click(clicked_id) {
@@ -20,12 +19,16 @@ document.addEventListener("scroll", () => {
         var elm = document.getElementById(articles[i]);
         if (posY >= sections[i] - trigger - nav_height) {
             elm.style.transition = "opacity 0.3s linear 0s";
+            // elm.style.transition = "width 0.3s linear 0s";
             elm.style.opacity = "95%";
+            // elm.style.width = "100%";
             if (articles[i] === "magazine_cards") { //opacity 100%인 요소들
                 elm.style.opacity = "100%";
+                // elm.style.width = "100%";
             }
         } else{
             elm.style.opacity = "0%";
+            // elm.style.width = "0%";
         }
     }
 })
